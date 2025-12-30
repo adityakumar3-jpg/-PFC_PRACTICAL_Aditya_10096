7657e1f33a2384d24c45f5bcfc48897fd0f0d452
adityakumar3-jpg <aditya.kumar3@rungta.org>
2025-12-30 11:25:04 +0530

    Add files via upload

--- practical 10/practical10-1.c
+++ practical 10/practical10-1.c
@@ -13,13 +13,20 @@ int main() { printf("Aditya_10096\n");
         }
     }
 
+    int max = arr[0][0];
+    int min = arr[0][0];
+
     for(i = 0; i < r; i++) {
-        int sum = 0;
         for(j = 0; j < c; j++) {
-            sum += arr[i][j];
+            if(arr[i][j] > max)
+                max = arr[i][j];
+            if(arr[i][j] < min)
+                min = arr[i][j];
         }
-        printf("Sum of row %d = %d\n", i + 1, sum);
     }
+
+    printf("Maximum = %d\n", max);
+    printf("Minimum = %d\n", min);
 
     return 0;
 }
